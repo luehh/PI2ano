e1276b77cefa33d517c003b9438fd68bc2977c2c
Octávio Casarini <octaviocasarini2.0@gmail.com>
2023-09-04 09:36:04 -0300

    bebe gigante gordo 40 anos

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,27 @@
     const express=require('express')
     const app = express();
+    app.use(express.json())
     app.listen(9000, () => console.log("OK"));
+
+    const mysql = require('mysql/promise')
+    const connection = mysql.createPool({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: ''
+    })
     
     app.get('/',(req,res)=>{
         res.send("Luisa");
+    })
+
+    const getAllPessoas = async () =>{
+        const [query] = await connection
+        .execute('select * from TestePessoa.Pessoa')
+        return query;
+    }
+
+    app.get('/pessoa', async (req,res)=>{
+        const consulta = await getAllPessoas();
+        return res.status(200).json(consulta);
     })
