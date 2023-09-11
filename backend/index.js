74b61d723b556de55414d18926f738d45741081a
Luisa Faria <luisa.f@aluno.ifsp.edu.br>
2023-09-11 09:32:09 -0300

    copy

--- backend/index.js
+++ backend/index.js
@@ -5,3 +5,22 @@
     app.get('/',(req,res)=>{
         res.send("Luisa");
     })
+
+    const getAllPessoas = async ()=>{
+        const [query] = await connection
+        .execute('select * from testepessoa.pessoa');
+        return query; 
+    }
+
+    app.get('/pesspa/:id', async (req,res)=>{
+        const {id} = req.params;
+        const [query] = await connection.execute('select * from TestePessoa.Pessoa where id = ?', [id]);
+        if(query.lenght === 0) return res.status(400).json({mensagem: 'Nao encontrado. '})
+        return res.status(200).json(query);
+        })
+
+    app.post('/pessoa', async (req,res)=>{
+        const {nome, email} = req.body
+        const [query]= await connection.execute('insert into TestePessoa.Pessoa (nome,email) values (?,?)', [nome,email])
+        return query
+        })
