b343b7d84754bada528ba60fa52ac14f7dc250bf
Luisa Faria <luisa.f@aluno.ifsp.edu.br>
2023-09-18 09:40:32 -0300

    methods

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
     const express=require('express')
     const app = express();
+    app.use(express.urlencoded({ extended: true}));
     app.use(express.json())
     app.listen(9000, () => console.log("OK"));
 
@@ -20,16 +21,40 @@
         .execute('select * from testepessoa.pessoa');
         return query; 
     }
-
-    app.get('/pesspa/:id', async (req,res)=>{
-        const {id} = req.params;
-        const [query] = await connection.execute('select * from TestePessoa.Pessoa where id = ?', [id]);
-        if(query.lenght === 0) return res.status(400).json({mensagem: 'Nao encontrado. '})
+    
+    app.get('/pessoa', async (req, res) => {
+        const [query] = await connection.execute('SELECT * FROM TestePessoa.Pessoa');
         return res.status(200).json(query);
-        })
-
-    app.post('/pessoa', async (req,res)=>{
-        const {nome, email} = req.body
-        const [query]= await connection.execute('insert into TestePessoa.Pessoa (nome,email) values (?,?)', [nome,email])
-        return query
-        })
+        });
+    
+    app.get('/pessoa/:id', async (req, res) => {
+        const { id } = req.params;
+        const [query] = await connection.execute('SELECT * FROM TestePessoa.Pessoa WHERE id = ?', [id]);
+        if (query.length === 0) return res.status(400).json({ mensagem: 'Não encontrado.' });
+        return res.status(200).json(query);
+        });
+    
+    app.get('/pessoa/busca/:nome', async (req, res) => {
+        const { nome } = req.params;
+        const [query] = await connection.execute('SELECT * FROM TestePessoa.Pessoa WHERE nome LIKE ?', [`%${nome}%`]);
+        if (query.length === 0) return res.status(400).json({ mensagem: 'Não encontrado.' });
+        return res.status(200).json(query);
+        });
+    
+    app.post('/pessoa', async (req, res) => {
+        const { nome, email } = req.body;
+        const [query] = await connection.execute('INSERT INTO TestePessoa.Pessoa (nome, email) VALUES (?, ?)', [nome, email]);
+        return res.status(200).json(query);
+        });
+    
+    app.put('/pessoa/:id', async (req, res) => {
+        const { id } = req.params;
+        const [query] = await connection.execute('UPDATE TestePessoa.Pessoa SET nome = ?, email = ? WHERE id = ?', [req.body.nome, req.body.email, id]);
+        return res.status(200).json(query);
+        });
+    
+    app.delete('/pessoa/:id', async (req, res) => {
+        const { id } = req.params;
+        const [query] = await connection.execute('DELETE FROM TestePessoa.Pessoa WHERE id = ?', [id]);
+        return res.status(200).json(query);
+        });
